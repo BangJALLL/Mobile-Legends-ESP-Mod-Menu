00cbd10d9d1465aef3219a160daf7505ca39ccbc
Bang IJAL <119733085+BangJALLL@users.noreply.github.com>
2023-02-09 17:37:57 +0700

    Delete Coloring Animation

--- app/src/main/java/uk/lgl/animation/AnimationSetupCallback.java
+++
@@ -1,9 +0,0 @@
-package uk.lgl.animation;
-
-import uk.lgl.modmenu.FloatingModMenuService;
-
-public interface AnimationSetupCallback {
-    public void onSetupAnimation(TitanicTextView titanicTextView);
-    public void onSetupAnimation(TitanicButton Button);
-}
-
